a1d37da522e696b210cd8e213d9b90b161a13616
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2026-05-03 11:36:54 +0100

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = numbers.Length - 1;
+
+        while (rightIndex >= leftIndex)
+        {
+            var sum = numbers[leftIndex] + numbers[rightIndex];
+            if (sum == target)
+                return [leftIndex + 1, rightIndex + 1];
+
+            if (sum > target)
+                rightIndex--;
+            else
+                leftIndex++;
+        }
+
+        return null;
+    }
+}
