7d2d063cab800ca69a8251a060ad8d7001d5cd1f
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2026-05-07 20:47:08 +0100

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public int MaxAreaOfIsland(int[][] grid) {
+         var rows = grid.Length;
+        var cols = grid[0].Length;
+
+        var queue = new Queue<(int x, int y)>();
+        (int dx, int dy)[] directions = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        var max = 0;
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == 0)
+                    continue;
+
+                var sum = 0;
+                grid[i][j] = 0;
+                queue.Enqueue((i, j));
+
+                while (queue.Count > 0)
+                {
+                    var (currentX, currentY) = queue.Dequeue();
+                    sum++;
+                    foreach (var (directionX, directionY) in directions)
+                    {
+                        var nextX = currentX + directionX;
+                        var nextY = currentY + directionY;
+
+                        if (nextX < 0 || nextX >= rows || nextY < 0 || nextY >= cols || grid[nextX][nextY] == 0)
+                            continue;
+
+                        grid[nextX][nextY] = 0;
+                        queue.Enqueue((nextX, nextY));
+                    }
+                }
+
+                if (sum > max)
+                    max = sum;
+            }
+        }
+
+        return max;
+    }
+}
