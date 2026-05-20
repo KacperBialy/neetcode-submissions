7c891ee5636b260e75d32002b8a5d2a51e73318c
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2026-05-20 21:42:12 +0100

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int HammingWeight(uint n) {
+        uint sum = 0;
+
+        while (n != 0)
+        {
+            sum += n & 1;
+            n >>= 1;
+        }
+
+        return (int)sum;
+    }
+}
